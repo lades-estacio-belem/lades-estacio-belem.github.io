15a57005f6736312a226875773a279c86c9a0b55
José Augusto <jq.quaresma12@gmail.com>
2024-05-03 19:34:23 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // prevent default form submission
+    // You can add your form submission logic here, such as sending data to a server
+    alert('Formulário enviado com sucesso!');
+});
